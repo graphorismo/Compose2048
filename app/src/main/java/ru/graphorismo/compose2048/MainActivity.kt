5cd1039b6030b3a9d4a9d0432b0529c7e2f60112
graphorismo <graphorismo@gmail.com>
2022-11-20 15:48:50 +0300

    Init commit. Jetpack compose empty project.

---
+++ app/src/main/java/ru/graphorismo/compose2048/MainActivity.kt
@@ -0,0 +1,43 @@
+package ru.graphorismo.compose2048
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import ru.graphorismo.compose2048.ui.theme.Compose2048Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Compose2048Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Compose2048Theme {
+        Greeting("Android")
+    }
+}
