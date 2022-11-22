0836503c56e848b865f83209a80074e6b451a296
graphorismo <graphorismo@gmail.com>
2022-11-22 15:59:10 +0300

    Collected move performers under a facade with a small exception check.

---
+++ app/src/main/java/ru/graphorismo/compose2048/domain/MovePerformersFacade.kt
@@ -0,0 +1,36 @@
+package ru.graphorismo.compose2048.domain
+
+class MovePerformersFacade(
+    private var moveUpPerformer: MoveUpPerformer,
+    private var moveDownPerformer: MoveDownPerformer,
+    private var moveLeftPerformer: MoveLeftPerformer,
+    private var moveRightPerformer: MoveRightPerformer
+) {
+
+    fun performMoveUp(matrix: MutableList<MutableList<Int>>){
+        throwIfNotMatrix(matrix)
+        moveUpPerformer.execute(matrix)
+    }
+
+    fun performMoveDown(matrix: MutableList<MutableList<Int>>){
+        throwIfNotMatrix(matrix)
+        moveDownPerformer.execute(matrix)
+    }
+
+    fun performMoveLeft(matrix: MutableList<MutableList<Int>>){
+        throwIfNotMatrix(matrix)
+        moveLeftPerformer.execute(matrix)
+    }
+
+    fun performMoveRight(matrix: MutableList<MutableList<Int>>){
+        throwIfNotMatrix(matrix)
+        moveRightPerformer.execute(matrix)
+    }
+
+    private fun throwIfNotMatrix(matrix: MutableList<MutableList<Int>>){
+        val linesLengths = matrix.map { it.size }
+        val matrixWidth = linesLengths[0]
+        if(linesLengths.any{it != matrixWidth } || matrix.size!=matrixWidth)
+            throw RuntimeException("MovePerformersFacade cant handle not a square matrices")
+    }
+}
