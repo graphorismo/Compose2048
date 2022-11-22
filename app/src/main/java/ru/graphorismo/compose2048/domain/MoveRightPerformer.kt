e1176be338894a44141f492ba78ede99ebf6830f
graphorismo <graphorismo@gmail.com>
2022-11-22 09:23:38 +0300

    Move code from MoveLeftPerformer into MoveRightPerformer because it has a "move right" logic instead of "move left"

--- app/src/main/java/ru/graphorismo/compose2048/domain/MoveRightPerformer.kt
+++ app/src/main/java/ru/graphorismo/compose2048/domain/MoveRightPerformer.kt
@@ -2,6 +2,22 @@ package ru.graphorismo.compose2048.domain
 
 class MoveRightPerformer {
     fun execute(list: MutableList<MutableList<Int>>){
-
+        for(line in list){
+            var somethingWasDone = true
+            while (somethingWasDone){
+                somethingWasDone = false
+                for (i in (line.size-2) downTo  0){
+                    if (line[i+1]==0 && line[i]!=0){
+                        line[i+1] = line[i]
+                        line[i] = 0
+                        somethingWasDone = true
+                    }else if (line[i+1] == line[i]  && line[i+1]!=0){
+                        line[i] = 0
+                        line[i+1] *= 2
+                        somethingWasDone = true
+                    }
+                }
+            }
+        }
     }
 }
