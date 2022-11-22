60b1be1b1d5013b3b888a97a988c8656ccabbd96
graphorismo <graphorismo@gmail.com>
2022-11-22 14:59:00 +0300

    Add logic in MoveDownPerformer and tests for it

--- app/src/main/java/ru/graphorismo/compose2048/domain/MoveDownPerformer.kt
+++ app/src/main/java/ru/graphorismo/compose2048/domain/MoveDownPerformer.kt
@@ -1,7 +1,32 @@
 package ru.graphorismo.compose2048.domain
 
+import androidx.core.util.rangeTo
+
 class MoveDownPerformer {
-    fun execute(list: MutableList<MutableList<Int>>){
+
+    fun execute(matrix: MutableList<MutableList<Int>>){
+        var matrixWidth = matrix[0].size
+        var matrixHeight = matrix.size
+        for (i in 0 until matrixWidth){
+            var somethingWasDone = true
+            while (somethingWasDone){
+                somethingWasDone = false
+                for (j in (matrixHeight-2) downTo 0){
+                    if(matrix[j+1][i]==0 && matrix[j][i] != 0){
+                        matrix[j+1][i] = matrix[j][i]
+                        matrix[j][i] = 0
+                        somethingWasDone = true
+                    }else if (matrix[j][i] == matrix[j+1][i]  && matrix[j+1][i]!=0){
+                        matrix[j][i] = 0
+                        matrix[j+1][i] *= 2
+                        somethingWasDone = true
+                    }
+
+                }
+            }
+        }
+
 
     }
+
 }
--- app/src/test/java/ru/graphorismo/compose2048/MoveDownPerformerTest.kt
+++ app/src/test/java/ru/graphorismo/compose2048/MoveDownPerformerTest.kt
@@ -1,4 +1,78 @@
 package ru.graphorismo.compose2048
 
+import org.junit.Assert
+import org.junit.Test
+import ru.graphorismo.compose2048.domain.MoveDownPerformer
+import ru.graphorismo.compose2048.domain.MoveLeftPerformer
+
 class MoveDownPerformerTest {
+
+    // [ [0,0,0,0],     [ [0,0,0,0],
+    //   [0,0,0,2],  ->   [2,0,0,2],
+    //   [0,0,2,4],       [0,0,2,4],
+    //   [0,2,4,8] ]      [0,2,4,8] ]
+    @Test
+    fun test1(){
+        var field = mutableListOf(
+            mutableListOf(0,0,0,0),
+            mutableListOf(0,0,0,2),
+            mutableListOf(0,0,2,4),
+            mutableListOf(0,2,4,8)
+        )
+        val performer = MoveDownPerformer()
+        performer.execute(field)
+        val expectedResult = mutableListOf(
+            mutableListOf(0,0,0,0),
+            mutableListOf(0,0,0,2),
+            mutableListOf(0,0,2,4),
+            mutableListOf(0,2,4,8)
+        )
+        Assert.assertTrue(expectedResult == field)
+    }
+
+    // [ [4,0,0,0],     [ [0,0,0,0],
+    //   [0,0,2,0],  ->   [0,0,0,0],
+    //   [0,4,0,2],       [0,0,0,0],
+    //   [4,4,2,2] ]      [8,8,4,4] ]
+    @Test
+    fun test2(){
+        var field = mutableListOf(
+            mutableListOf(4,0,0,0),
+            mutableListOf(0,0,2,0),
+            mutableListOf(0,4,0,2),
+            mutableListOf(4,4,2,2)
+        )
+        val performer = MoveDownPerformer()
+        performer.execute(field)
+        val expectedResult = mutableListOf(
+            mutableListOf(0,0,0,0),
+            mutableListOf(0,0,0,0),
+            mutableListOf(0,0,0,0),
+            mutableListOf(8,8,4,4)
+        )
+        Assert.assertTrue(expectedResult == field)
+    }
+
+    // [ [0,2,0,4],     [ [0,0,0,0],
+    //   [2,2,4,2],  ->   [0,0,0,4],
+    //   [2,2,0,0],       [2,0,0,2],
+    //   [2,2,4,4] ]      [4,8,8,4] ]
+    @Test
+    fun test3(){
+        var field = mutableListOf(
+            mutableListOf(0,2,0,4),
+            mutableListOf(2,2,4,2),
+            mutableListOf(2,2,0,0),
+            mutableListOf(2,2,4,4)
+        )
+        val performer = MoveDownPerformer()
+        performer.execute(field)
+        val expectedResult = mutableListOf(
+            mutableListOf(0,0,0,0),
+            mutableListOf(0,0,0,4),
+            mutableListOf(2,0,0,2),
+            mutableListOf(4,8,8,4)
+        )
+        Assert.assertTrue(expectedResult == field)
+    }
 }
