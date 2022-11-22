612ac09e2669e3372d4d6c8191325950d652cd35
graphorismo <graphorismo@gmail.com>
2022-11-22 09:47:17 +0300

    Add logic in MoveLeftPerformer and tests for it

---
+++ app/src/main/java/ru/graphorismo/compose2048/domain/MoveLeftPerformer.kt
@@ -0,0 +1,23 @@
+package ru.graphorismo.compose2048.domain
+
+class MoveLeftPerformer {
+    fun execute(list: MutableList<MutableList<Int>>){
+        for(line in list){
+            var somethingWasDone = true
+            while (somethingWasDone){
+                somethingWasDone = false
+                for (i in 1..(line.size-1)){
+                    if (line[i-1]==0 && line[i]!=0){
+                        line[i-1] = line[i]
+                        line[i] = 0
+                        somethingWasDone = true
+                    }else if (line[i-1] == line[i]  && line[i-1]!=0){
+                        line[i] = 0
+                        line[i-1] *= 2
+                        somethingWasDone = true
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ app/src/test/java/ru/graphorismo/compose2048/MoveLeftPerformerTest.kt
@@ -0,0 +1,77 @@
+package ru.graphorismo.compose2048
+
+import org.junit.Test
+import org.junit.Assert.*
+import ru.graphorismo.compose2048.domain.MoveLeftPerformer
+
+class MoveLeftPerformerTest {
+
+    // [ [0,0,0,0],     [ [0,0,0,0],
+    //   [2,0,0,0],  ->   [2,0,0,0],
+    //   [2,4,0,0],       [2,4,0,0],
+    //   [2,4,8,0] ]      [2,4,8,0] ]
+    @Test
+    fun test1(){
+        var field = mutableListOf(
+            mutableListOf(0,0,0,0),
+            mutableListOf(2,0,0,0),
+            mutableListOf(2,4,0,0),
+            mutableListOf(2,4,8,0)
+        )
+        val performer = MoveLeftPerformer()
+        performer.execute(field)
+        val expectedResult = mutableListOf(
+            mutableListOf(0,0,0,0),
+            mutableListOf(2,0,0,0),
+            mutableListOf(2,4,0,0),
+            mutableListOf(2,4,8,0)
+        )
+        assertTrue(expectedResult == field)
+    }
+
+    // [ [0,0,2,2],     [ [4,0,0,0],
+    //   [0,2,0,2],  ->   [4,0,0,0],
+    //   [0,0,4,4],       [8,0,0,0],
+    //   [4,0,0,4] ]      [8,0,0,0] ]
+    @Test
+    fun test2(){
+        var field = mutableListOf(
+            mutableListOf(0,0,2,2),
+            mutableListOf(0,2,0,2),
+            mutableListOf(0,0,4,4),
+            mutableListOf(4,0,0,4)
+        )
+        val performer = MoveLeftPerformer()
+        performer.execute(field)
+        val expectedResult = mutableListOf(
+            mutableListOf(4,0,0,0),
+            mutableListOf(4,0,0,0),
+            mutableListOf(8,0,0,0),
+            mutableListOf(8,0,0,0)
+        )
+        assertTrue(expectedResult == field)
+    }
+
+    // [ [2,2,2,0],     [ [4,2,0,0],
+    //   [2,2,2,2],  ->   [8,0,0,0],
+    //   [4,0,4,0],       [8,0,0,0],
+    //   [4,0,2,4] ]      [4,2,4,0] ]
+    @Test
+    fun test3(){
+        var field = mutableListOf(
+            mutableListOf(2,2,2,0),
+            mutableListOf(2,2,2,2),
+            mutableListOf(4,0,4,0),
+            mutableListOf(4,0,2,4)
+        )
+        val performer = MoveLeftPerformer()
+        performer.execute(field)
+        val expectedResult = mutableListOf(
+            mutableListOf(4,2,0,0),
+            mutableListOf(8,0,0,0),
+            mutableListOf(8,0,0,0),
+            mutableListOf(4,2,4,0)
+        )
+        assertTrue(expectedResult == field)
+    }
+}
